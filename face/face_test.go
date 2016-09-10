217ff27687c004ad9c0e2c53eadb1d5e71218979
Ahmadreza Zibaei <ahmdrz@users.noreply.github.com>
2016-09-10 19:46:31 +0430

    Testing new method

--- face/face_test.go
+++ face/face_test.go
@@ -6,3 +6,11 @@ import (
 )
 
 var key string = "-" // this code is sample , forget this :) my token has been changed !
+
+func TestNew(t *testing.T) {
+	_,err := New(key)
+	if err != nil {
+		t.Log(err)
+		return
+	}
+}
